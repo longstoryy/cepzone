1b824c7f66573401515a354d5d6471a96802067d
long story <115299302+longstoryy@users.noreply.github.com>
2024-06-15 05:17:44 -0700

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -91,8 +91,12 @@ AUTH_USER_MODEL = 'accounts.Account'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'bonmLSSvcMjXaWWJBNYWeBWYsUdROWCu',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '16473'
     }
 }
 
