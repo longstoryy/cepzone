929d6f0bf54cd3cdec508de24be883e3cc15fbda
Japhet Adjetey <adjetadjetey45@gmail.com>
2024-06-29 15:07:09 -0700

    project modified

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+# DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
